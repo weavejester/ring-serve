7df052cbb49000ec4c0471109e8d8be07599a13d
James Reeves <jreeves@weavejester.com>
2011-04-08 17:24:57 -0400

    Updated Ring version

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject ring-serve "0.1.0"
   :description "Ring development web server"
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [ring/ring-devel "0.3.5"]
-                 [ring/ring-jetty-adapter "0.3.5"]])
+                 [ring/ring-devel "0.3.7"]
+                 [ring/ring-jetty-adapter "0.3.7"]])
